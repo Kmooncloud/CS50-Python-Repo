1e14930ffedfbf7c8b7835c5531c17cf8309235f
Kiri Maasen <23128445+Kmooncloud@users.noreply.github.com>
2022-11-23 18:54:10 -0800

    Added indoor and playback from pset 0

---
+++ indoor/indoor.py
@@ -0,0 +1,5 @@
+# pset0 Indoor Voice
+# return input in all lowercase 
+
+phrase = input("Why are you angry? ")
+print(phrase.lower())
---
+++ playback/playback.py
@@ -0,0 +1,5 @@
+# pset 0 Playback Speed
+# return input with ellipses between each word 
+
+phrase = input()
+print(phrase.replace(" ", "..."))
