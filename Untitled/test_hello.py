41ddb7fd5ba53800e223e8d8c30a419b5e91043c
Kiri Maasen <23128445+Kmooncloud@users.noreply.github.com>
2022-12-16 15:31:34 -0800

    reorg of notes folder

--- Untitled/test_hello.py
+++
@@ -1,8 +0,0 @@
-from notes.hello import hello
-
-def test_default():
-    assert hello() == "hello, world"
-
-def test_arguemnt():
-    for name in ["Hermione", "Harry", "Ron"]:
-        assert hello(name) == f"hello, {name}"
--- notes/costumes.py
+++ notes/costumes.py
@@ -6,7 +6,7 @@ images = []
 
 for arg in sys.argv[1:]:
     image = Image.open(arg)
-    image.append(image)
+    images.append(image)
 
 images[0].save(
     "costumes.gif", save_all=True, append_images=[images[1]], duration=200, loop=0
